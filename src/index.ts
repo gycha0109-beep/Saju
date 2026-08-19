04b716e85c7a958ca72305f5069a4f9a24a43e4c
gycha0109-beep <gycha0109@gmail.com>
2026-08-19 10:38:59 +0900

    Export deterministic rule evaluator

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './contracts/index.js';
 export * from './calculation/calculation-engine.js';
 export * from './interpretation/rule-registry.js';
 export * from './interpretation/execution-plan.js';
+export * from './interpretation/rule-evaluator.js';
