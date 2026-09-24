8aa5fab5c9f02a4a0acb737e084cb1af29f418e3
gycha0109-beep <gycha0109@gmail.com>
2026-09-24 10:38:08 +0900

    research(face-reading): add Android depth capability probe (FR273)
    Closes #1401
    
    Watchtower-Track: face-research

---
+++ tools/fr273-android-depth-probe/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.myeongha.fr273depthprobe"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.myeongha.fr273depthprobe"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
